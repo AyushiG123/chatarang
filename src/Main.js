f1d8fc34f6e3701b45ab2a6502f0f2c2ce389dd3
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-07-03 09:39:35 -0400

    Add main component

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class Main extends Component{
+    render()
+    {
+        return(
+            <div className = "Main">
+               Main
+            </div>
+        )
+    }
+}
+
+export default Main
