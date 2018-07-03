bd33ec7df1ef3a81294272df10869b30b260ab90
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-07-03 09:56:17 -0400

    Styled Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import './App.css'
 import Main from './Main'
 
 class App extends Component {
--- src/Main.js
+++ src/Main.js
@@ -7,12 +7,18 @@ class Main extends Component{
     render()
     {
         return(
-            <div className = "Main">
+            <div className = "Main" style = {styles}>
                <Sidebar />
                <Chat />
             </div>
         )
     }
 }
+
+const styles = {
+    display: 'flex',
+    alignItems: 'stretch',
+    height: '100vh',
+   }
 
 export default Main
